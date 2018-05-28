38a90774961dfe15e9acd1647fec57d7b82d6839
R221 <rcjames1111@gmail.com>
2018-05-28 15:52:14 -0400

    Added members to staff class

--- Staff.cs
+++ Staff.cs
@@ -3,8 +3,47 @@ namespace Payroll
 {
     public class Staff
     {
-        public Staff()
+        private float hourlyRate;
+        private int hWorked;
+
+
+        float TotalPay { get; set}
+        float BasicPay { get; set}
+        string NameOfStaff { get; set}
+        public int HoursWorked
         {
+            get
+            {
+                return hWorked;
+            }
+            set
+            {
+                if (HoursWorked > 0)
+                    return value;
+
+                else
+                    hWorked = 0;
+            }
+        }
+
+        public Staff(string name, float rate)
+        {
+            NameOfStaff = name;
+            hourlyRate = rate;
+        }
+
+        public virtual CalculatePay()
+        {
+            Console.WriteLine("Calculating Pay...");
+            BasicPay = hWorked * hourlyRate;
+            TotalPay = BasicPay;
+        }
+
+        public override string ToString()
+        {
+            return "Name of staff: " + NameOfStaff + 
+                ",hourlyRate: " + hourlyRate + 
+                ", hWorked: " + hWorked;
         }
     }
 }
