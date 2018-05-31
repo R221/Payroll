8081540b892ae24b067d77b8df1273152619c1bc
R221 <rcjames1111@gmail.com>
2018-05-30 21:05:15 -0400

    Added Admin.cs

--- Admin.cs
+++ Admin.cs
@@ -3,6 +3,36 @@ namespace Payroll
 {
     public class Admin : Staff
     {
-        public string admin1;
+        private float const overtimeRate = 15.5;
+        private float const adminHourlyRate = 30;
+
+        private float Overtime { get;set; }
+
+        public Admin(string name) : base(name, adminHourlyRate)
+        {
+        }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+            TotalPay = BasicPay;
+            Overtime = overtimeRate * (HoursWorked - 160);
+
+            if (HoursWorked > 160)
+            {
+                TotalPay = BasicPay + Overtime;
+            }
+            else
+            {
+                TotalPay = BasicPay;
+            }   
+        }
+
+        public override string ToString()
+        {
+            return "Name of staff: " + NameOfStaff +
+            ",hourlyRate: " + hourlyRate +
+            ",hWorked: " + hWorked;
+        } 
     }
 }
